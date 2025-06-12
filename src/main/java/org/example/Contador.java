465e988f0272be080acaac094cb1db2ac295562c
Luan <luanmvcosta@gmail.com>
2025-06-12 11:47:06 -0300

    First commit

---
+++ src/main/java/org/example/Contador.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.print("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+    }
+}
